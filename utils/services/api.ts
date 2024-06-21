70a14bd037009e946c3b70cb600fe79b9f9aa1e8
Marie <mje.espinosa@gmail.com>
2024-06-21 15:07:24 +0200

    connect to db

---
+++ utils/services/api.ts
@@ -0,0 +1,14 @@
+export async function fetchData(url : string, options = {}) {
+    console.log(url)
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
