87e5cf57fa5aebda70c95a20c593a093d9ec52cb
Marie <mje.espinosa@gmail.com>
2024-06-23 19:29:46 +0200

    use server and disable sharing with client

--- utils/services/recipes.ts
+++ utils/services/recipes.ts
@@ -1,7 +1,8 @@
+"use server"
 import { Hit, Recipe } from "@/types/Recipe";
 import { fetchData } from "./api";
 
-const initUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.NEXT_PUBLIC_RECIPE_ID}&app_key=${process.env.NEXT_PUBLIC_RECIPE_KEY}&mealType=Dinner`;
+const initUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}&mealType=Dinner`;
 
 export async function getMeals(url = initUrl) {
     try {
@@ -39,7 +40,7 @@ export async function getMeals(url = initUrl) {
 
 export async function getRecipeById(id : string) {
     try {
-        const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=${process.env.NEXT_PUBLIC_RECIPE_ID}&app_key=${process.env.NEXT_PUBLIC_RECIPE_KEY}`;
+        const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}`;
         const res = await fetchData(url);  
         return res.recipe as Recipe;
     } catch (error) {
