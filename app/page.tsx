31d410fb41ca4e25144487e47933d557fa58aa06
Marie <mje.espinosa@gmail.com>
2024-06-23 19:30:22 +0200

    new location of recipes list

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,7 @@
-import RecipesList from "@/components/organism/RecipesList";
-import { getMeals } from "@/utils/services/recipes";
-
 export default async function Index() {
-  // retrieve all recipes
-  const res = await getMeals();
-
-  if (res && res.length > 0) {
-
-    return (
-      <>
-  <input
-    type="text"
-    id="Search"
-    placeholder="Search for..."
-    className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
-  /> 
-      <RecipesList recipesList={res}/>
-
-      </>
-    );
-
-  }
-
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      No data available
-    </div>
-  );
+  return(
+    <main>
+      Hello
+    </main>
+  )
 }
