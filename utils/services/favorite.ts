bbfab052159305f9e39827f08b1286e293e94f64
Marie <mje.espinosa@gmail.com>
2024-06-23 19:32:27 +0200

    add and remove a product from favorites

--- utils/services/favorite.ts
+++ utils/services/favorite.ts
@@ -1,4 +1,5 @@
-import { createClient } from "../supabase/server";
+"use server"
+import { createClient } from "../supabase/client"
 
 export async function getFavorites(){
     try {
@@ -7,6 +8,50 @@ export async function getFavorites(){
         return results 
     } catch (error) {
         console.log(error)
+    }
+    
+}
+
+export async function AddToFavorites(meal_id : string, comments : string){
+    try {
+        const database = createClient()
+        const {error} = await database.from('favorites').insert({ meal_id, comments})
+        return error 
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+export async function removeFromFavorites(meal_id : string){
+    try {
+        const database = createClient()
+        const {error} = await database.from('favorites').delete().eq('meal_id', meal_id)
+        return error 
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+
+export async function isFavorite(meal_id : string){
+    try {
+        const database = createClient()
+        const { data, error } = await database
+        .from('favorites')
+        .select()
+        .eq('meal_id', meal_id)
+
+        if (error) {
+            console.log(error);
+            return false;
+        }
+
+        return data && data.length > 0
+    } catch (error) {
+        console.log(error)
+        return false;
     }
     
 }
