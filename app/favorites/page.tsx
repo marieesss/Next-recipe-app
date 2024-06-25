8992559cbe4499a7c25e7651b8d9c61dea071661
Marie <mje.espinosa@gmail.com>
2024-06-25 23:14:59 +0200

    fix bug on retrieving favorites

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,8 +1,13 @@
-import Favorites from "@/components/layout/Favorites"
-import { getFavorites } from "@/utils/services/favorite"
-import { getRecipesFavorites } from "@/utils/services/recipes"
+import Favorites from "@/components/layout/Favorites";
+import { getRecipesFavorites } from "@/utils/services/recipes";
+import { Recipe } from '@/types/Recipe';
 
 export default async function Page() {
-    const data = await getRecipesFavorites()   
-    if(data) return <Favorites data={data}/>
-  }
+    const data: Recipe[] | Error = await getRecipesFavorites();
+
+    if (data instanceof Error) {
+        return <div>Error: {data.message}</div>;
+    }
+
+    return <Favorites data={data} />;
+}
